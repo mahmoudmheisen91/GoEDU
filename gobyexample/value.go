7590482b4662ce275bf1595580144330b3c02613
Mahmoud <mahmoudmheisen91@gmail.com>
2020-06-12 23:41:36 +0300

    add basic scripts

---
+++ gobyexample/value.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("ans =", 1+2)
+	fmt.Println("test"+"num=", 10)
+	fmt.Println(true && false)
+}
---
+++ gobyexample/vars.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a = 5
+	var b int8 = 6
+	c := 7
+	fmt.Println(a, b, c)
+}
